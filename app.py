360cb143bc3873ba26166ebd6124e5edb72f35b4
Harshvardhan Jadhav <jadhavharshvardhan6301@gmail.com>
2025-04-01 15:35:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+# app.py
+
+import streamlit as st
+
+# Title of the app
+st.title("Trade Checklist App")
+
+# Introduction
+st.write("Welcome to the Trade Checklist App! Please follow the checklist before entering a trade.")
